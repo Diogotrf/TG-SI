356993a631cd2a8126cd213897c9db058eabbf8e
Afonso Martins <afonsomartinspaivamiguel@gmail.com>
2024-04-16 09:26:58 +0100

    Adição de exeplo de feth aquando um get

---
+++ Main/Home/home.js
@@ -0,0 +1,12 @@
+//Exemplo para panhar dados da API
+//fetch('/Registo/Edit/data')
+//    .then(response => response.json())
+//    .then(data => {
+//        dados=data
+//        console.log(data);
+//    })
+//    .catch(error => {
+//        // lidar com o erro, se houver
+//        console.log(data)
+//        console.log(error);
+//    });
